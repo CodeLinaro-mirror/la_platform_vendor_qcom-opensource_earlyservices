7849ecb4f12bd81bef7fe83cc291d9bfdaec1832
Pankaj Kumar <pankkuma@codeaurora.org>
2019-10-17 14:47:01 +0530

    [early-services] : Adding support for early architecture
    1.Added new files early_init.cpp, utils, log and test.c
    2.test.c is a test application invoked from early_init.conf
    3.early_init.cpp main() will be called when kernel calls
    init_early binary
    4.early_init has both first stage and second stage like
    Android init.
    	a.First stage will load sepolicy
    	b.Second stage will execute test app
    
    Change-Id: I66f3a560fc27efd7a97ef80c27e2c884a68de8e3
    Signed-by: Pankaj Kumar <pankkuma@codeaurora.org>

--- early-init/Android.mk
+++ early-init/Android.mk
@@ -2,12 +2,33 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES:= early_init.c
+LOCAL_SRC_FILES:= early_init.cpp \
+                  util.cpp \
+                  log.cpp
+
 LOCAL_MODULE := init_early
 LOCAL_STATIC_LIBRARIES := libc++_static
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(LOCAL_MODULE_PATH)/early_services;
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/earlyrootfs
+LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(TARGET_ROOT_OUT)/early_services;
+LOCAL_STATIC_LIBRARIES := \
+     libbase \
+     libseccomp_policy \
+     libselinux \
+     liblog \
+     libcrypto_utils \
+     libcrypto \
+
+LOCAL_CPPFLAGS := -std=c++17
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+#To be removed later
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := init_early_test
+LOCAL_SRC_FILES := test.c
+LOCAL_MODULE_CLASS = ETC
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/earlyrootfs/system/bin
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -20,6 +41,10 @@ LOCAL_MODULE_PATH := $(PRODUCT_OUT)/earlyrootfs/etc
 LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(LOCAL_MODULE_PATH)/../sbin; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../system/bin; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../system/lib64; \
+                        mkdir -p $(LOCAL_MODULE_PATH)/../system/etc; \
+                        mkdir -p $(LOCAL_MODULE_PATH)/../vendor/lib64; \
+                        mkdir -p $(LOCAL_MODULE_PATH)/../vendor/lib; \
+                        mkdir -p $(LOCAL_MODULE_PATH)/../system/etc/selinux; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../run/early; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../proc; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../sys; \
@@ -28,6 +53,8 @@ LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(LOCAL_MODULE_PATH)/../sbin; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../usr/sbin; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../vendor/firmware_mnt; \
 			mkdir -p $(LOCAL_MODULE_PATH)/../vendor/lib/modules; \
+                        mkdir -p $(LOCAL_MODULE_PATH)/../vendor/etc; \
+                        mkdir -p $(LOCAL_MODULE_PATH)/../vendor/etc/selinux; \
 			ln -sf system/bin $(LOCAL_MODULE_PATH)/../bin; \
 			ln -sf system/lib64 $(LOCAL_MODULE_PATH)/../lib64; \
 
