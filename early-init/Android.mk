5cd760c5dacc20e39a9b1d5672943687038f8668
Pankaj Kumar <pankkuma@codeaurora.org>
2020-06-11 17:31:49 +0530

    early_services: Added flag for backward compatibility
    early_services should be mounted as a part of ramdisk in Q
    but not in P.
    
    Change-Id: Ib84f854fb037d34adbfc1fdac4491a318f348ac6

--- early-init/Android.mk
+++ early-init/Android.mk
@@ -10,8 +10,10 @@ LOCAL_MODULE := init_early
 LOCAL_STATIC_LIBRARIES := libc++_static
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/early_services
-LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(TARGET_ROOT_OUT)/early_services; \
-                          mkdir -p $(TARGET_RAMDISK_OUT)/early_services;
+LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(TARGET_ROOT_OUT)/early_services;
+ifneq ($(BOARD_BUILD_SYSTEM_ROOT_IMAGE),true)
+  LOCAL_POST_INSTALL_CMD += mkdir -p $(TARGET_RAMDISK_OUT)/early_services;
+endif
 LOCAL_STATIC_LIBRARIES := \
      libbase \
      libseccomp_policy \
@@ -30,8 +32,9 @@ LOCAL_MODULE := init_early_test
 LOCAL_SRC_FILES := test.c
 LOCAL_MODULE_CLASS = ETC
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/early_services/system/bin
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-
+ifneq ($(BOARD_BUILD_SYSTEM_ROOT_IMAGE),true)
+  LOCAL_FORCE_STATIC_EXECUTABLE := true
+endif
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
