6987eb5dda51f6ad75457ac2c385c2fe0895b9e5
Sunish Nambayil <quic_snambayi@quicinc.com>
2023-05-26 10:19:35 +0530

    ES: Set audioreach by default
    Set audioreach by default.
    
    Change-Id: I37e9a4006e592a17e8bb92e42a1843456b4f5fb2
    Signed-off-by: Sunish Nambayil <quic_snambayi@quicinc.com>

--- early-init/early_init.cpp
+++ early-init/early_init.cpp
@@ -852,7 +852,7 @@ bool bc_get_lmp() {
 }
 
 bool bc_get_ar() {
-  bool audio_reach = false;
+  bool audio_reach = true;
   android::earlyinit::import_kernel_bootconfig(false,
      [&](const std::string& key, const std::string& value, bool in_qemu) {
     (void)in_qemu;
