1c0f295aee6bb335f5a500d0a6ae315c308d8a58
Raghavendra Prasad N <quic_raghnaga@quicinc.com>
2023-07-28 09:32:39 +0530

    earlyservices: Change boot kpi markers
    Change boot kpi markers to redirect to a log file.
    
    Change-Id: Ibdede2f7d3b005b6ef3d7461600a30c5d196a6b3
    Signed-off-by: Raghavendra Prasad N <quic_raghnaga@quicinc.com>

--- early-init/early_chime_app.c
+++ early-init/early_chime_app.c
@@ -31,6 +31,41 @@
 ** DAMAGE.
 **/
 
+/*
+* Changes from Qualcomm Innovation Center are provided under the following license:
+* Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted (subject to the limitations in the
+* disclaimer below) provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*
+*     * Redistributions in binary form must reproduce the above
+*       copyright notice, this list of conditions and the following
+*       disclaimer in the documentation and/or other materials provided
+*       with the distribution.
+*
+*     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+*       contributors may be used to endorse or promote products derived
+*       from this software without specific prior written permission.
+*
+* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <tinyalsa/asoundlib.h>
 #include <errno.h>
 #include <stdio.h>
@@ -150,6 +185,7 @@ try_again:
 
 void place_marker(char const *name)
 {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0))
    int fd=open("/sys/kernel/boot_kpi/kpi_values", O_WRONLY);
    if (fd > 0)
    {
@@ -158,6 +194,14 @@ void place_marker(char const *name)
        write(fd, earlyapp, strlen(earlyapp));
        close(fd);
    }
+#else
+   int fd = freopen("/dev/kmsg", "w", stdout);
+   if (fd > 0)
+   {
+       printf("boot_kpi: %s\n", name);
+       close(fd);
+   }
+#endif
 }
 
 void play_sample(FILE *file, unsigned int card, unsigned int device, unsigned int channels,
